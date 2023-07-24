d9cd3bb5f4835a7147c276ff157b028048e79035
Stepan Andreev <stepan10andreev@gmail.com>
2023-07-24 14:53:18 +0300

    add favicon and add some adaptive styles

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,20 +1,27 @@
-import { Header } from "@/components/Header/Header";
-import { UserInfo } from "@/components/Header/UserInfo/UserInfo";
-import { UserInfoContent } from "@/components/UserInfoContent/UserInfoContent";
 import { UserScreen } from "@/components/screens/UserScreen";
-import { Container } from "@/components/ui-components/Container/Container";
-import { Content } from "@/components/ui-components/Content/Content";
-import { Title } from "@/components/ui-components/Title/Title";
-import { UIButton } from "@/components/ui-components/UIButton/UIButton";
-import { UIText } from "@/components/ui-components/UIText/UIText";
-import { USERS_PAGE_HEADER_DESRIPTION } from "@/constants/textConstants";
 import { UsersService } from "@/services/users.service";
+
+type MetaDataProps = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+  { params, searchParams }: MetaDataProps
+) {
+  const id = params.id
+
+  return {
+    title: `User ${id} | Profile`,
+    description: `Профиль пользователя ${id}`,
+  }
+}
 
 export default async function UserPage({ params }: { params: { id: string } }) {
   const { id } = params;
   const userData = await UsersService.getUsersByID(id);
 
   return (
-    <UserScreen userData={userData}/>
+    <UserScreen userData={userData} />
   )
 }
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,15 +1,10 @@
-import { Header } from "@/components/Header/Header";
-import { useAppDispatch, useAppSelector } from "@/components/Hooks/useApp";
-import { UserList } from "@/components/UsersList/UserList";
 import { UsersListScreen } from "@/components/screens/UsersListScreen";
-import { Container } from "@/components/ui-components/Container/Container";
-import { Content } from "@/components/ui-components/Content/Content";
-import { Title } from "@/components/ui-components/Title/Title";
-import { UIButton } from "@/components/ui-components/UIButton/UIButton";
-import { UIText } from "@/components/ui-components/UIText/UIText";
-import { USERS_PAGE_HEADER_DESRIPTION } from "@/constants/textConstants";
-import { UsersService } from "@/services/users.service";
-import { Suspense } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Users | List',
+  description: 'Список пользвоателей',
+}
 
 export default function UsersListPage() {
   return (
